70ccd718f6fc141a17755842800728228507b2a7
KevinCanacuanPasquel <kfc0_10@hotmail.com>
2018-11-13 08:35:27 -0500

    Taller10

---
+++ OCJP-Deberes/src/Deber4_Ejercicios/Taller10.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Deber4_Ejercicios;
+
+/**
+ *
+ * @author kfc0_
+ */
+public class Taller10 {
+    public static void main(String[] args) {
+        int i=1;
+        int j=2;
+        int k=3;
+        int m=2;
+        System.out.println ((j >= i) || (k == m));
+        
+        double x = Math.pow(2, 2);
+        System.out.println (x);
+    }
+}
